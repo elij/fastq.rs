51a26fe135dbb938f2547cb8784f2b08c8c7a9e6
Elijah Charles <elijah.charles@intel.com>
2015-05-05 10:50:33 +0100

    Added additional tests
    Added tests to cover file without trailing newline

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ mod tests {
   use super::parser::*;  
 
   #[test]
-  fn test1() {
+  fn should_parse_two_reads() {
     let header = "SRR316957.252299 B086EABXX110425:3:1102:20344:169356/1";
     let seq =
 &b"@SRR316957.252299 B086EABXX110425:3:1102:20344:169356/1
@@ -25,7 +25,22 @@ DFBDFBG@DGBE=EBFFFEFFFFCF/9>89@??:?CCDCCGG=EEDEBF8?@BBC?D;EDEACEEEEBED?=FGGF
 "[..];
 
     let reads = many_reads(seq);
-    println!("{:?}", reads);  
+    match reads {
+      IResult::Done(_, o) => assert_eq!(header, o[0].id),
+      _ => assert!(false)
+    }
+  }
+
+  #[test]
+  fn should_parse_one_read_without_newline() {
+    let header = "SRR316957.252299 B086EABXX110425:3:1102:20344:169356/1";
+    let seq =
+&b"@SRR316957.252299 B086EABXX110425:3:1102:20344:169356/1
+CAGACACAAACCTTTCTTTGTGTGGAGCTCCCACGGTAAAAAGACCATTGTCAAGTGCATGTATATAGGTTCCCTC
++
+DFBDFBG@DGBE=EBFFFEFFFFCF/9>89@??:?CCDCCGG=EEDEBF8?@BBC?D;EDEACEEEEBED?=FGGF"[..];
+
+    let reads = many_reads(seq);
     match reads {
       IResult::Done(_, o) => assert_eq!(header, o[0].id),
       _ => assert!(false)
